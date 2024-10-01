7690dd1e1f74200acd82d829f94e9debf13e5524
tajroshith <tajroshith@gmail.com>
2024-10-01 12:19:55 +0530

    Updated InstanceCreation.tf

--- terraformscripts/InstanceCreation.tf
+++ terraformscripts/InstanceCreation.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "webserver" {
     ami                    =   "ami-05a5bb48beb785bf1" 
     instance_type          =   "t2.micro"
     key_name               =   "Devkops" 
-    vpc_security_group_ids = ["sg-0c6a05f35651f66f7"]
+    vpc_security_group_ids = ["sg-06773bf58a6fd7bf8"]
     tags = {
         Name = "tomcatservers"
     } 
